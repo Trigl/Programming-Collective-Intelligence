2c383680bf7b9c946a7dad9ab070f15a8ff60a84
Trigl <18810690994@163.com>
2017-05-26 19:37:33 +0800

    形成文章和高频词矩阵

--- Chapter3/generatefeedvector.py
+++ Chapter3/generatefeedvector.py
@@ -1,4 +1,6 @@
+# coding: utf-8
 import feedparser
+# 正则
 import re
 
 
@@ -23,4 +25,43 @@ def getwordcounts(url):
 
 def getwords(html):
 	# 去除所有HTML标记
-	
+	txt = re.compile(r'<[^>]+>').sub('', html)
+
+	# 利用所有非字母字符拆分出单词
+	words = re.compile(r'[^A-Z^a-z]+').split(txt)
+
+	# 转化成小写形式
+	return [word.lower() for word in words if word != '']
+
+
+apcount = {}
+wordcounts = {}
+# 遍历所有博客
+feedlist = [line for line in file('feedlist.txt')]
+for feedurl in feedlist:
+	title, wc = getwordcounts(feedurl)
+	wordcounts[title] = wc
+	for word, count in wc.items():
+		apcount.setdefault(word, 0)
+		# 统计单词出现在几篇博客里
+		if count > 1:
+			apcount[word] += 1
+
+# 统计高频词
+wordlist = {}
+for w, bc in apcount.items():
+	frac = float(bc) / len(feedlist)
+	if frac > 0.1 and frac < 0.5: wordlist.append(w)
+
+out = file('blogdata.txt', 'w')
+out.write('Blog')
+# 每一列对应一个单词
+for word in wordlist: out.write('\t%s' % word)
+out.write('\n')
+for blog, wc in wordcounts.items():
+	# 每一行对应一个博客名
+	out.write(blog)
+	for word in wordlist:
+		if word in wc: out.write('\t%d' % wc[word])
+		else: out.write('\t0')
+	out.write('\n')
